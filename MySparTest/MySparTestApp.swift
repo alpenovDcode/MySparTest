2af2e8ef64bd969c8d0294ca377239535b06f7a8
alpenovDcode <alpenovevgeniy05@gmail.com>
2024-02-01 19:17:56 +0600

    Initial Commit

---
+++ MySparTest/MySparTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestApp.swift
+//  MySparTest
+//
+//  Created by Альпеша on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
